9c91bb81eeb3ab42227158ce6f7d03900f64c116
Krzysztof Sułkowski <krzysztof.sulkowski@studenci.collegiumwitelona.pl>
2025-05-22 23:22:03 +0200

    Changed version for the Capsule Management Admin Panel

--- TimeCapsule/TimeCapsule/Controllers/Admin/CapsuleManagementController.cs
+++ TimeCapsule/TimeCapsule/Controllers/Admin/CapsuleManagementController.cs
@@ -1,18 +1,86 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TimeCapsule.Controllers;
+using TimeCapsule.Models.DatabaseModels;
+using TimeCapsule.Models.Dto;
+using TimeCapsule.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace TimeCapsule.Controllers.Admin
+
+[Authorize(Roles = "Admin")]
+[Route("AdminPanel/Capsules")]
+public class CapsuleManagementController : TimeCapsuleBaseController
 {
-    public class CapsuleManagementController : TimeCapsuleBaseController
-    {
-        [Authorize(Roles = "Admin")]
-        [Route("AdminPanel/Capsules")]
+    private readonly TimeCapsuleContext _context;
 
-        [HttpGet]
-        [ApiExplorerSettings(IgnoreApi = true)]
-        public IActionResult GetCapsules()
+    public CapsuleManagementController(TimeCapsuleContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("")]
+    public IActionResult GetCapsules()
+    {
+        var listaKapsul = _context.Capsules
+            .Include(c => c.CreatedByUser)
+            .Where(c => c.Status != Status.Deleted)
+            .Select(c => new AdminCapsuleDto
+            {
+                CapsuleId = c.Id,
+                Title = c.Title,
+                UserName = c.CreatedByUser.Email,
+                IsOpened = c.Status == Status.Opened,
+                OpenDate = c.OpeningDate
+            })
+            .ToList();
+
+        return View("~/Views/CapsuleManagement/CapsulesManagementView.cshtml", listaKapsul);
+    }
+
+    [HttpGet("Edit/{id}")]
+    public IActionResult EditCapsuleDate(int id)
+    {
+        var capsule = _context.Capsules.FirstOrDefault(c => c.Id == id);
+
+        if (capsule == null)
+            return NotFound();
+
+        var model = new AdminCapsuleDto
         {
-            return View();
+            CapsuleId = capsule.Id,
+            OpenDate = capsule.OpeningDate
+        };
+
+        return View("~/Views/CapsuleManagement/EditCapsuleDate.cshtml", model);
+    }
+
+    [HttpPost("Edit")]
+    public IActionResult EditCapsuleDate(AdminCapsuleDto model)
+    {
+        var capsule = _context.Capsules.FirstOrDefault(c => c.Id == model.CapsuleId);
+
+        if (capsule == null)
+            return NotFound();
+
+        capsule.OpeningDate = model.OpenDate;
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(GetCapsules));
+    }
+
+    [HttpPost("Delete/{id}")]
+    public IActionResult DeleteCapsule(int id)
+    {
+        var capsule = _context.Capsules.FirstOrDefault(c => c.Id == id);
+
+        if (capsule == null)
+        {
+            return NotFound();
         }
+
+        capsule.Status = Status.Deleted;
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(GetCapsules));
     }
 }
---
+++ TimeCapsule/TimeCapsule/Models/Dto/AdminCapsuleDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TimeCapsule.Models.Dto
+{
+    public class AdminCapsuleDto
+    {
+        public int CapsuleId { get; set; }
+        public string Title { get; set; }
+        public string UserName { get; set; }
+        public bool IsOpened { get; set; }
+        public DateTime OpenDate { get; set; }
+    }
+}
